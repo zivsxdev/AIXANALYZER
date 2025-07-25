3bcd75a100de944c5a1dd53211054c65e8e124d0
TAUKIL AHMED <zivsxdev@gmail.com>
2025-07-26 03:42:22 +0530

    Cloud storage, PDF-Image conversion, key-Value storage and AI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";              // ✅ Needed for useEffect
+import { useNavigate } from "react-router"; // ✅ Needed for navigation
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import {resumes} from "../../constants";
+import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter" 
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,6 +15,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const {auth} = usePuterStore();
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate("/auth?next=/");
+    }, [auth.isAuthenticated])
+
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover]">
     <Navbar/>
      <section className="main-section">
